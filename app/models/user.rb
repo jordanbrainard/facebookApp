60f7c90ce0ba8f5f418203d266dd70d5e57f2315
Jordan Brainard <35605866+jordanbrainard@users.noreply.github.com>
2018-03-08 11:47:52 -0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   def self.koala(auth)
     access_token = auth['token']
     facebook = Koala::Facebook::API.new(access_token)
-    facebook.get_object("me?fields=name,picture,movies,birthday,gender,relationship")
+    facebook.get_object("me?fields=name,picture,movies,birthday,gender,relationship_status,significant_other")
   end
 
 end
